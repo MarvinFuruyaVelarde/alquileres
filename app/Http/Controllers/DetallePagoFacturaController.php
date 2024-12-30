fd443d532626a0533030e24473d8f0848b410046
Marvin Furuya <marvinfuruya@gmail.com>
2024-12-30 09:59:16 -0400

    Adición de flag para ver si se cobra Mora en Registro de Pago

--- app/Http/Controllers/DetallePagoFacturaController.php
+++ app/Http/Controllers/DetallePagoFacturaController.php
@@ -32,8 +32,11 @@ class DetallePagoFacturaController extends Controller
         $fechaInicial = Carbon::createFromDate($request->gestion, $request->mes, 1)->startOfDay();
         $fechaLimite = $fechaInicial->addMonth()->day(10)->startOfDay();
         $diasMora = 0;
+        $cobroMora = 'N';
+
         if (Carbon::parse($request->fecha_actual)->startOfDay() > $fechaLimite){
             $diasMora = Carbon::parse($request->fecha_actual)->startOfDay()->diffInDays($fechaLimite->startOfDay());
+            $cobroMora='S';
         }
         $factura=Factura::find($request->factura_id);
         $pagado_saldo=DetallePagoFactura::where('id_factura',$request->factura_id)->latest('id') ->first();
@@ -62,6 +65,7 @@ class DetallePagoFacturaController extends Controller
         $detalle_pago->numero_registro_deposito=$request->registro_deposito;
         $detalle_pago->numero_registro_cobro=$request->recibo_cobro;
         $detalle_pago->observacion=$request->observacion;
+        $detalle_pago->cobro_mora = $cobroMora;
         $detalle_pago->save();
 
         Alert::success("Registro de Pago registrado correctamente");
