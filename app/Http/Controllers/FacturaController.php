5796d6bab18770000abbeadd337860eb1c2fadd5
Marvin Furuya <marvinfuruya@gmail.com>
2025-01-06 19:28:15 -0400

    Depuración error al generar factura

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -341,6 +341,7 @@ class FacturaController extends Controller
             Alert::success("Se ha procesado de manera correcta la generación de factura(s)");
             return redirect()->route('facturacion.index');
         } else {
+            dd($response->json());
             Alert::error("Ocurrio un inconveniente en la generación de factura(s)");
             return redirect()->route('facturacion.index');
         }
