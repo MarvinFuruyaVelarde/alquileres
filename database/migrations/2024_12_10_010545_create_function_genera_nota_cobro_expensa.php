161ee171fb24ec42e45e86a1c9df6f80a694b5fb
Marvin Furuya <marvinfuruya@gmail.com>
2025-01-08 17:15:44 -0400

    Corrección de Generación de Notas de Cobro Expensas

--- database/migrations/2024_12_10_010545_create_function_genera_nota_cobro_expensa.php
+++ database/migrations/2024_12_10_010545_create_function_genera_nota_cobro_expensa.php
@@ -13,54 +13,55 @@ return new class extends Migration
     {
         DB::statement("
             CREATE OR REPLACE FUNCTION public.genera_nota_cobro_expensa(
-	aeropuerto_id integer,
-	ultimo_dia date)
-    RETURNS TABLE(id_espacio integer, id_contrato integer, codigo_contrato character varying, id_cliente integer, tipo_solicitante integer, ci character varying, nit character varying, razon_social character varying, tarifa_fija character varying, aeropuerto integer, id_forma_pago integer, forma_pago character varying, expensa integer, monto numeric) 
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
+                aeropuerto_id integer,
+                ultimo_dia date)
+                RETURNS TABLE(id_espacio integer, id_contrato integer, codigo_contrato character varying, id_cliente integer, tipo_solicitante integer, ci character varying, nit character varying, razon_social character varying, tarifa_fija character varying, aeropuerto integer, id_forma_pago integer, forma_pago character varying, expensa integer, monto numeric) 
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
+                ROWS 1000
 
-AS $$
-DECLARE
-    primer_dia DATE := date_trunc('month', ultimo_dia);
-BEGIN
-    RETURN QUERY
-    SELECT 
-        E.id AS id_espacio,
-        E.contrato AS id_contrato, 
-        C.codigo AS codigo_contrato,                    
-        CL.id AS id_cliente,
-        C.tipo_solicitante,
-        C.ci,
-        C.nit,
-        CL.razon_social,
-        EE.tarifa_fija, 
-        C.aeropuerto, 
-        FP.id AS id_forma_pago,
-        FP.descripcion AS forma_pago,
-        EE.expensa,
-        EE.monto
-    FROM 
-        ESPACIO E
-    INNER JOIN 
-        ESPACIO_EXPENSA EE ON EE.ESPACIO = E.ID  
-    INNER JOIN 
-        CONTRATO C ON C.ID = E.contrato
-    INNER JOIN 
-        CLIENTE CL ON CL.ID = C.CLIENTE
-    INNER JOIN 
-        forma_pago FP ON FP.id = E.forma_pago
-    WHERE 
-        E.COBRO_EXPENSA = 'S'
-        AND EE.TARIFA_FIJA = 'F'
-        AND C.estado = 5
-        AND E.estado = 5
-        AND C.aeropuerto = aeropuerto_id
-        AND (primer_dia <= E.fecha_final AND ultimo_dia >= E.fecha_inicial)
-	ORDER BY CL.razon_social; 
-END;
-$$;
+            AS $$
+            DECLARE
+                primer_dia DATE := date_trunc('month', ultimo_dia);
+            BEGIN
+                RETURN QUERY
+                SELECT 
+                    E.id AS id_espacio,
+                    E.contrato AS id_contrato, 
+                    C.codigo AS codigo_contrato,                    
+                    CL.id AS id_cliente,
+                    C.tipo_solicitante,
+                    C.ci,
+                    C.nit,
+                    CL.razon_social,
+                    EE.tarifa_fija, 
+                    C.aeropuerto, 
+                    FP.id AS id_forma_pago,
+                    FP.descripcion AS forma_pago,
+                    EE.expensa,
+                    EE.monto
+                FROM 
+                    ESPACIO E
+                INNER JOIN 
+                    ESPACIO_EXPENSA EE ON EE.ESPACIO = E.ID  
+                INNER JOIN 
+                    CONTRATO C ON C.ID = E.contrato
+                INNER JOIN 
+                    CLIENTE CL ON CL.ID = C.CLIENTE
+                INNER JOIN 
+                    forma_pago FP ON FP.id = E.forma_pago
+                WHERE 
+                    E.COBRO_EXPENSA = 'S'
+                    AND EE.TARIFA_FIJA = 'F'
+                    AND C.estado = 5
+                    AND E.estado = 5
+                    AND EE.DELETED_AT IS NULL
+                    AND C.aeropuerto = aeropuerto_id
+                    AND (primer_dia <= E.fecha_final AND ultimo_dia >= E.fecha_inicial)
+                ORDER BY CL.razon_social; 
+            END;
+            $$;
         ");
     }
 
--- database/migrations/2024_12_10_011526_create_function_obtener_expensas_tarifa_variable.php
+++ database/migrations/2024_12_10_011526_create_function_obtener_expensas_tarifa_variable.php
@@ -13,43 +13,44 @@ return new class extends Migration
     {
         DB::statement("
         CREATE OR REPLACE FUNCTION public.obtener_expensas_tarifa_variable(
-	p_aeropuerto integer,
-	p_cliente integer,
-	p_codigo character varying,
-	p_fecha_inicial date,
-	p_fecha_final date)
-    RETURNS TABLE(id_contrato integer, id_espacio integer, detalle_espacio text, expensa integer, descripcion character varying, factor numeric, unidad_medida character varying, tarifa_fija character varying) 
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
+            p_aeropuerto integer,
+            p_cliente integer,
+            p_codigo character varying,
+            p_fecha_inicial date,
+            p_fecha_final date)
+            RETURNS TABLE(id_contrato integer, id_espacio integer, detalle_espacio text, expensa integer, descripcion character varying, factor numeric, unidad_medida character varying, tarifa_fija character varying) 
+            LANGUAGE 'plpgsql'
+            COST 100
+            VOLATILE PARALLEL UNSAFE
+            ROWS 1000
 
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT C.ID AS id_contrato,
-           E.ID AS id_espacio,
-		   CONCAT('POR UNA CANTIDAD DE ', e.cantidad, ' ', u.descripcion, ' DURANTE ', e.fecha_inicial, ' AL ', e.fecha_final, 
-                  ' CON UNA GARANTIA DE ', e.garantia, ' CON UN CANON MENSUAL DE ', e.total_canonmensual) AS detalle_espacio,
-           EE.EXPENSA,
-	       EX.DESCRIPCION,
-	       EX.FACTOR,
-	 	   EX.UNIDAD_MEDIDA,
-           EE.TARIFA_FIJA
-      FROM CONTRATO C
-     INNER JOIN ESPACIO E ON E.CONTRATO = C.ID
-     INNER JOIN ESPACIO_EXPENSA EE ON EE.ESPACIO = E.ID
-     INNER JOIN UNIDAD_MEDIDA U ON U.ID = E.UNIDAD_MEDIDA 
-	 INNER JOIN EXPENSA EX ON EX.ID = EE.EXPENSA  
-     WHERE C.ESTADO = 5
-       AND E.ESTADO = 5
-       AND EE.TARIFA_FIJA = 'V'
-       AND C.AEROPUERTO = p_aeropuerto
-       AND C.CLIENTE = p_cliente
-       AND C.CODIGO = p_codigo
-       AND (p_fecha_inicial <= E.fecha_final AND p_fecha_final >= E.fecha_inicial);
-END;
-$$;
+        AS $$
+        BEGIN
+            RETURN QUERY
+            SELECT C.ID AS id_contrato,
+                E.ID AS id_espacio,
+                CONCAT('POR UNA CANTIDAD DE ', e.cantidad, ' ', u.descripcion, ' DURANTE ', e.fecha_inicial, ' AL ', e.fecha_final, 
+                        ' CON UNA GARANTIA DE ', e.garantia, ' CON UN CANON MENSUAL DE ', e.total_canonmensual) AS detalle_espacio,
+                EE.EXPENSA,
+                EX.DESCRIPCION,
+                EX.FACTOR,
+                EX.UNIDAD_MEDIDA,
+                EE.TARIFA_FIJA
+            FROM CONTRATO C
+            INNER JOIN ESPACIO E ON E.CONTRATO = C.ID
+            INNER JOIN ESPACIO_EXPENSA EE ON EE.ESPACIO = E.ID
+            INNER JOIN UNIDAD_MEDIDA U ON U.ID = E.UNIDAD_MEDIDA 
+            INNER JOIN EXPENSA EX ON EX.ID = EE.EXPENSA  
+            WHERE C.ESTADO = 5
+            AND E.ESTADO = 5
+            AND EE.TARIFA_FIJA = 'V'
+            AND EE.DELETED_AT IS NULL
+            AND C.AEROPUERTO = p_aeropuerto
+            AND C.CLIENTE = p_cliente
+            AND C.CODIGO = p_codigo
+            AND (p_fecha_inicial <= E.fecha_final AND p_fecha_final >= E.fecha_inicial);
+        END;
+        $$;
     ");
     }
 
